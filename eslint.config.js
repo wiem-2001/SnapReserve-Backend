5ced30febff8f59070049db7742c632d7392a1a3
wiem-2001 <wiembm2001@gmail.com>
2025-07-29 12:03:12 +0100

    ignore linting prisma generated files

--- eslint.config.js
+++ eslint.config.js
@@ -2,24 +2,32 @@ import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] ,
-  rules: {
-      "no-unused-vars": "warn",
-      "no-console": "warn",
-      "eqeqeq": "error",
-      "curly": "error",
-      "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 0 }],
-      "no-eval": "error",
-      "no-implied-eval": "error",
-      "no-new-func": "error",
-      "prefer-const": "warn",
-      "no-constant-condition": "warn",
-      "max-len": ["error", { "code": 300 }],
-      "max-lines": ["error", { "max": 500 }],
-      "no-duplicate-imports": "error",
-      
-    }
-},
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
-]);
+export default defineConfig({
+  ignores: ["src/generated/**"],  // <--- ignore this path globally
+  overrides: [
+    {
+      files: ["**/*.{js,mjs,cjs}"],
+      plugins: { js },
+      extends: ["js/recommended"],
+      rules: {
+        "no-unused-vars": "warn",
+        "no-console": "warn",
+        "eqeqeq": "error",
+        "curly": "error",
+        "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 0 }],
+        "no-eval": "error",
+        "no-implied-eval": "error",
+        "no-new-func": "error",
+        "prefer-const": "warn",
+        "no-constant-condition": "warn",
+        "max-len": ["error", { "code": 300 }],
+        "max-lines": ["error", { "max": 500 }],
+        "no-duplicate-imports": "error",
+      },
+    },
+    {
+      files: ["**/*.{js,mjs,cjs}"],
+      languageOptions: { globals: globals.node },
+    },
+  ],
+});
