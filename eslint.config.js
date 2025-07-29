442389b54024a8b122e95e5af7102dec4da6c4fb
wiem-2001 <wiembm2001@gmail.com>
2025-07-29 12:40:43 +0100

    remove flat config in eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,32 +2,33 @@ import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
-export default defineConfig({
-  ignores: ["src/generated/**"],  // <--- ignore this path globally
-  overrides: [
-    {
-      files: ["**/*.{js,mjs,cjs}"],
-      plugins: { js },
-      extends: ["js/recommended"],
-      rules: {
-        "no-unused-vars": "warn",
-        "no-console": "warn",
-        "eqeqeq": "error",
-        "curly": "error",
-        "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 0 }],
-        "no-eval": "error",
-        "no-implied-eval": "error",
-        "no-new-func": "error",
-        "prefer-const": "warn",
-        "no-constant-condition": "warn",
-        "max-len": ["error", { "code": 300 }],
-        "max-lines": ["error", { "max": 500 }],
-        "no-duplicate-imports": "error",
-      },
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: globals.node,
     },
-    {
-      files: ["**/*.{js,mjs,cjs}"],
-      languageOptions: { globals: globals.node },
+    plugins: {
+      js,
     },
-  ],
-});
+    rules: {
+      ...js.configs.recommended.rules, 
+      "no-unused-vars": "warn",
+      "no-console": "warn",
+      "eqeqeq": "error",
+      "curly": "error",
+      "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 0 }],
+      "no-eval": "error",
+      "no-implied-eval": "error",
+      "no-new-func": "error",
+      "prefer-const": "warn",
+      "no-constant-condition": "warn",
+      "max-len": ["error", { code: 300 }],
+      "max-lines": ["error", { max: 500 }],
+      "no-duplicate-imports": "error",
+    },
+  },
+  {
+    ignores: ["src/generated/**"],
+  },
+]);
